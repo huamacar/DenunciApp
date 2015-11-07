fb0a883d95ba46da0db802c6f5d6aa872ac2b0bb
Deariel <dennisarielper@gmail.com>
2015-11-07 00:01:09 -0600

    Servicio Denuncia

--- App/servicios/getDenuncias.php
+++ App/servicios/getDenuncias.php
@@ -1,11 +1,19 @@
 <?php
 	include ('Conexion.php');
-	$sql = "SELECT nombre FROM usuario";
+	$sql = "SELECT * FROM Denuncia";
 	$conn->set_charset("utf8");
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
     	while($row = $result->fetch_assoc()) {
-        	echo "<div>" . $row["nombre"] . "</div>";
+			
+			echo "<div class='col-lg-4 col-md-4 col-xs-6 denuncia'>"
+            echo "<img src='img/registro-icon.png' class='img-circle' width='100' height='100'>"
+            echo "<h3>" . $row["titulo"] . "</h3>"
+            echo "<h4>" . $row["fecha"] . "</h4>"
+            echo "<h5>" . $row["ubicacion"] . "</h5>"
+            echo "<h4>" . $row["valoracion"] . "</h4>"
+            echo "<input type='button' class='btn btn-primary' value='Ver Detalles'>"
+            echo "</div>";
     	}
 	}
 ?>
